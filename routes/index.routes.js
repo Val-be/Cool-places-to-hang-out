cb69dce669105be358efd1b5222b573adff2dfa1
gaurav sharma <g11.sharma@gmail.com>
2022-06-13 12:40:23 +0200

    fixes from testing auth

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const authRoutes = require("./auth.routes");
 const usersRoutes = require("./users.routes");
 const placesRoutes = require("./places.routes");
 const favoritesRoutes = require("./favorites.routes");
@@ -11,6 +12,7 @@ router.get("/", (req, res, next) => {
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
 
+router.use("/auth", authRoutes);
 router.use("/users", usersRoutes);
 router.use("/places", placesRoutes);
 router.use("/favorites", favoritesRoutes);
