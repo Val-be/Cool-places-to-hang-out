6d2fe224cdc7f2b5d2a38edafb81431f2bee6dbe
Valérian Belin <belinvalerian26@gmail.com>
2022-06-14 12:41:28 +0200

    routes fixed

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -3,7 +3,7 @@ const { Schema, model, SchemaTypes } = require('mongoose');
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const commentSchema = new Schema(
   {
-    Text: {
+    text: {
       type: String,
       // unique: true -> Ideally, should be unique, but its up to you
     },
