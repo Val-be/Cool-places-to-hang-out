3114942732cb6c5c2410798a01c594121acb5ca7
Valérian Belin <belinvalerian26@gmail.com>
2022-06-14 17:00:30 +0200

    added protection to password change

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,17 +1,15 @@
 const jsonwebtoken = require('jsonwebtoken');
 const User = require('../models/User.model');
 
-const isAdmin = () => {
-  return async (req, res, next) => {
-    const authorization = req.headers.authorization;
-    const token = authorization.replace('Bearer ', '');
-    const decodedJwt = jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
-    const { username } = decodedJwt;
-    const user = await User.findOne({ username });
-    req.user = user;
-    if (req.user.role === 'admin') next();
-    else res.status(403).json({ message: 'You are not authorized' });
-  };
+const isAdmin = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  const token = authorization.replace('Bearer ', '');
+  const decodedJwt = jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
+  const { username } = decodedJwt;
+  const user = await User.findOne({ username });
+  req.user = user;
+  if (req.user.role === 'admin') next();
+  else res.status(403).json({ message: 'You are not authorized' });
 };
 
 module.exports = isAdmin;
